25223f775bfede365b4d01e8225e07e0cbdf8b20
DayanaNurdinova <130187491+DayanaNurdinova@users.noreply.github.com>
2023-04-11 19:41:00 +0600

    выполнила DZ2

---
+++ DZ2_dayana_25-01js/index.js
@@ -0,0 +1,32 @@
+// var arr = ['0','1','2','3','4','5','6','7','8','9','10']
+// for (var i = 0; i < arr.length; i++)
+//     console.log(arr[i])
+
+//
+// var message = prompt("Веберите цвет светофора")
+//
+// if (message === "Red") {
+//     alert("Stop!")
+// } else if (message === "Yellow") {
+//     alert("Wait")
+// } else if (message === "Green") {
+//     alert("GO")
+// } else {
+//     alert("правильно введите цвет")
+// }
+
+
+
+// for (var i = 20; i>=0; i--) {
+//     console.log(i)
+// }
+
+//
+// var array = [10, 20, 30, 50, 235, 3000]
+//
+// array.forEach((array) => {
+//     perv = String(array)[0]
+//     if (["1", "2", "5"].includes(perv)) {
+//         console.log(array)
+//     }
+// })
